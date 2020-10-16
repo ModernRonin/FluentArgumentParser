a6e2f4ae746b37f4f57d634ea011a2195576319b
Mark Hajek <markhajek@outlook.com>
2020-10-16 20:15:35 +0100

    xml documentation comments for all extension points

--- ModernRonin.FluentArgumentParser/Extensibility/IArgumentPreprocessor.cs
+++ ModernRonin.FluentArgumentParser/Extensibility/IArgumentPreprocessor.cs
@@ -1,5 +1,15 @@
-﻿namespace ModernRonin.FluentArgumentParser.Extensibility
+﻿using ModernRonin.FluentArgumentParser.Definition;
+
+namespace ModernRonin.FluentArgumentParser.Extensibility
 {
+    /// <summary>
+    ///     <para>
+    ///         Implement this interface and set <see cref="ParserConfiguration.ArgumentPreprocessor" /> to your implementation
+    ///         if you want to pre-process arguments before they are fed into verbs.
+    ///     </para>
+    ///     One example use case for this would be if you are not happy with the quoting mechanism used by the combination of
+    ///     shell and CLR runtime and want to customize it.
+    /// </summary>
     public interface IArgumentPreprocessor
     {
         string Process(string what);
