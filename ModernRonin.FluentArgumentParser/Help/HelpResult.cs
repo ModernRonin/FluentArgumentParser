2a5f7bd4fceb44c48af379cd98d742775af5f38c
Mark Hajek <markhajek@outlook.com>
2020-10-18 12:34:14 +0100

    added documentation for HelpResult

--- ModernRonin.FluentArgumentParser/Help/HelpResult.cs
+++ ModernRonin.FluentArgumentParser/Help/HelpResult.cs
@@ -1,8 +1,22 @@
-﻿namespace ModernRonin.FluentArgumentParser.Help
+﻿using ModernRonin.FluentArgumentParser.Parsing;
+
+namespace ModernRonin.FluentArgumentParser.Help
 {
+    /// <summary>
+    ///     Returned by <see cref="IBindingCommandLineParser.Parse" /> if the passed arguments contain an explicit request for
+    ///     help
+    ///     or if they were invalid.
+    /// </summary>
     public class HelpResult
     {
+        /// <summary>
+        ///     The help text to display to the user.
+        /// </summary>
         public string Text { get; set; }
+
+        /// <summary>
+        ///     True if the arguments parsed were invalid, false if they contained an explicit request for help.
+        /// </summary>
         public bool IsResultOfInvalidInput { get; set; }
     }
 }
