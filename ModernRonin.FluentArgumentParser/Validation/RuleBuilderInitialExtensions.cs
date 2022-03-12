8cfba40956459c37c8d921c4660ed22767177040
Mark Hajek <markhajek@outlook.com>
2022-03-12 17:27:03 +0000

    fixed .MustHaveDistinct()

--- ModernRonin.FluentArgumentParser/Validation/RuleBuilderInitialExtensions.cs
+++ ModernRonin.FluentArgumentParser/Validation/RuleBuilderInitialExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using FluentValidation;
 
 namespace ModernRonin.FluentArgumentParser.Validation;
@@ -10,6 +11,18 @@ public static class RuleBuilderInitialExtensions
     public static IRuleBuilderOptions<TContainer, IEnumerable<TElement>> MustHaveDistinct<TContainer,
         TElement, TSubProperty>(
         this IRuleBuilderInitial<TContainer, IEnumerable<TElement>> self,
-        Func<TElement, TSubProperty> propertySelector) =>
-        self.Must(vs => vs.Select(propertySelector).Distinct().Count() <= 1);
+        Expression<Func<TElement, TSubProperty>> propertySelector)
+    {
+        return self.Must(haveNoDuplicates)
+            .WithMessage($"Elements of collection must have distinct values for {propertySelector}");
+
+        bool haveNoDuplicates(TContainer container,
+            IEnumerable<TElement> elements,
+            ValidationContext<TContainer> context)
+        {
+            var subject = elements.ToArray();
+            if (subject.DistinctBy(propertySelector.Compile()).Count() == subject.Length) return true;
+            return false;
+        }
+    }
 }
